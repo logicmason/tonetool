a4e89f32627854a2f86d148749df71e784628789
Mark <wei.xiaoma@gmail.com>
2013-02-24 19:23:15 -0800

    adds tests

--- spec/toneSpec.js
+++ spec/toneSpec.js
@@ -10,24 +10,59 @@ describe("Tone Tool", function() {
     expect(addtones(input)).toEqual("");
   });
 
-  // describe("when song has been paused", function() {
-  //   beforeEach(function() {
-  //     player.play(song);
-  //     player.pause();
-  //   });
+  it("should correctly handle a single word", function(){
+    var input = "jue2ding4";
+    expect(addtones(input)).toEqual("juédìng");
+  });
 
-  //   it("should indicate that the song is currently paused", function() {
-  //     expect(player.isPlaying).toBeFalsy();
+  it("should correctly handle words with a v for a u umlaut", function(){
+    var input = ["lv4", "v2", "ce4lve4"];
+    var correctOutput = ["lǜ", "ǘ", "cèlüè"];
+    _(input).each(function(thing, i) {
+      console.log(correctOutput);
+      expect(addtones(thing)).toEqual(correctOutput[i]);
+    });
+  });
 
-  //     // demonstrates use of 'not' with a custom matcher
-  //     expect(player).not.toBePlaying(song);
-  //   });
+  it("should correctly handle words with a an existing ü", function(){
+    var input = ["lü4", "ü2", "ce4lüe4"];
+    var correctOutput = ["lǜ", "ǘ", "cèlüè"];
+    _(input).each(function(thing, i) {
+      console.log(correctOutput);
+      expect(addtones(thing)).toEqual(correctOutput[i]);
+    });
+  });
 
-  //   it("should be possible to resume", function() {
-  //     player.resume();
-  //     expect(player.isPlaying).toBeTruthy();
-  //     expect(player.currentlyPlayingSong).toEqual(song);
-  //   });
-  // });
+  it("should correctly handle words following neutral tones", function(){
+    var input = ["Wo3 bu2 ren4shi ta1", "Ta1men xian1 qu4 ba", "liang3 ge xuan3ze2"];
+    var correctOutput = ["Wǒ bú rènshi tā", "Tāmen xiān qù ba", "liǎng ge xuǎnzé"];
+    _(input).each(function(thing, i) {
+      console.log(correctOutput);
+      expect(addtones(thing)).toEqual(correctOutput[i]);
+    });
+  });
 
+  it("should correctly handle initial and retroflexive Rs", function(){
+    var input = ["ri4", "ru2guo3", "wanr2", "wan2r", "er4", "er2zi", "jia1renr2"];
+    var correctOutput = ["rì", "rúguǒ", "wánr", "wánr", "èr", "érzi", "jiārénr"];
+    _(input).each(function(thing, i) {
+      console.log(correctOutput);
+      expect(addtones(thing)).toEqual(correctOutput[i]);
+    });
+  });
+
+  it("should add a hyphen where necessary", function(){
+    expect(addtones("Xi1an1")).toEqual("Xī'ān");
+    expect(addtones("Ren2ai4")).toEqual("Rén'ài");
+    expect(addtones("hai3ou1")).toEqual("hǎi'ōu");
+  });
+
+  it("should correctly handle a complex sentence", function(){
+    var input = ["Ao4men2 wan2r wanr2 Zhong1guo2 Xi1an1 nv2er2 weir4"];
+    var correctOutput = ["Àomén wánr wánr Zhōngguó Xī'ān nǘ'ér wèir"];
+    _(input).each(function(thing, i) {
+      console.log(correctOutput);
+      expect(addtones(thing)).toEqual(correctOutput[i]);
+    });
+  });
 });

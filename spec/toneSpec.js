211217518c0a691a15caadf06cce81630c5ba86c
Mark <wei.xiaoma@gmail.com>
2013-02-24 16:33:52 -0800

    Initial commit of 2013 refactor

---
+++ spec/toneSpec.js
@@ -0,0 +1,33 @@
+describe("Tone Tool", function() {
+  var phrase;
+
+  beforeEach(function() {
+
+  });
+
+  it("should return empty for an empty string", function() {
+    var input = "";
+    expect(addtones(input)).toEqual("");
+  });
+
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(player.isPlaying).toBeFalsy();
+
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+
+});
